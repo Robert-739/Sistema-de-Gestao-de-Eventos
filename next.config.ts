69dfe818ce6f45daa097476030d4d99229127ac4
Robert Pereira <robert.willian.leal@gmail.com>
2026-05-27 16:07:30 -0300

    fix: adicionando configuracao de trace de fontes no next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* Se você já tiver outras configurações aqui dentro, mantenha-as e adicione apenas a linha abaixo */
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/certificado': ['./src/assets/fonts/**/*'],
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
